fc83cebce48d6d27d2c8bf739d17648b5ca4cace
silver14056 <silver14056@mail.ru>
2026-01-19 12:36:58 +0300

    Add Sphere and Scripts

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    private const float DirectionX = 0f;
-    private const float DirectionY = 0f;
-    private const float DirectionZ = -0.005f;
+    [SerializeField] private Vector3 _movementDirection;
 
     void Update()
     {
-        Vector3 movementDirection = new Vector3(DirectionX, DirectionY, DirectionZ);
-        
-        transform.Translate(movementDirection);
-
+        transform.Translate(_movementDirection, Space.World);
     }
 }
